7432d79149f3851d66ff2949205b21cd59e57e17
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-01-04 11:07:12 +0100

    create helper function for decodetransaction for external usage

--- dcrlibwallet.go
+++ dcrlibwallet.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
-	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -19,7 +18,6 @@ import (
 	"time"
 
 	"github.com/decred/dcrd/addrmgr"
-	"github.com/decred/dcrd/blockchain/stake"
 	"github.com/decred/dcrd/chaincfg"
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec"
@@ -46,8 +44,6 @@ import (
 var shutdownRequestChannel = make(chan struct{})
 var shutdownSignaled = make(chan struct{})
 var signals = []os.Signal{os.Interrupt, syscall.SIGTERM}
-
-const BlockValid int = 1 << 0
 
 type LibWallet struct {
 	dataDir       string
@@ -737,10 +733,10 @@ func (lw *LibWallet) TransactionNotification(listener TransactionListener) {
 				}
 				tempTransaction := Transaction{
 					Fee:       int64(transaction.Fee),
-					Hash:      fmt.Sprintf("%02x", reverse(transaction.Hash[:])),
+					Hash:      transaction.Hash.String(),
 					Raw:       fmt.Sprintf("%02x", transaction.Transaction[:]),
 					Timestamp: transaction.Timestamp,
-					Type:      transactionType(transaction.Type),
+					Type:      txhelper.TransactionType(transaction.Type),
 					Credits:   tempCredits,
 					Amount:    amount,
 					Height:    -1,
@@ -757,7 +753,7 @@ func (lw *LibWallet) TransactionNotification(listener TransactionListener) {
 			for _, block := range v.AttachedBlocks {
 				listener.OnBlockAttached(int32(block.Header.Height), block.Header.Timestamp.UnixNano())
 				for _, transaction := range block.Transactions {
-					listener.OnTransactionConfirmed(fmt.Sprintf("%02x", reverse(transaction.Hash[:])), int32(block.Header.Height))
+					listener.OnTransactionConfirmed(transaction.Hash.String(), int32(block.Header.Height))
 				}
 			}
 		}
@@ -850,10 +846,10 @@ func (lw *LibWallet) GetTransactionRaw(txHash []byte) (*Transaction, error) {
 
 	return &Transaction{
 		Fee:       int64(txSummary.Fee),
-		Hash:      fmt.Sprintf("%02x", reverse(txSummary.Hash[:])),
+		Hash:      txSummary.Hash.String(),
 		Raw:       fmt.Sprintf("%02x", txSummary.Transaction[:]),
 		Timestamp: txSummary.Timestamp,
-		Type:      transactionType(txSummary.Type),
+		Type:      txhelper.TransactionType(txSummary.Type),
 		Credits:   credits,
 		Amount:    amount,
 		Height:    height,
@@ -932,10 +928,10 @@ func (lw *LibWallet) GetTransactionsRaw() (transactions []*Transaction, err erro
 			}
 			tempTransaction := &Transaction{
 				Fee:       int64(transaction.Fee),
-				Hash:      fmt.Sprintf("%02x", reverse(transaction.Hash[:])),
+				Hash:      transaction.Hash.String(),
 				Raw:       fmt.Sprintf("%02x", transaction.Transaction[:]),
 				Timestamp: transaction.Timestamp,
-				Type:      transactionType(transaction.Type),
+				Type:      txhelper.TransactionType(transaction.Type),
 				Credits:   tempCredits,
 				Amount:    amount,
 				Height:    height,
@@ -967,128 +963,15 @@ func (lw *LibWallet) DecodeTransaction(txHash []byte) (string, error) {
 		log.Error(err)
 		return "", err
 	}
-	serializedTx := txSummary.Transaction
-	var mtx wire.MsgTx
-	err = mtx.Deserialize(bytes.NewReader(serializedTx))
+
+	tx, err := txhelper.DecodeTransaction(hash, txSummary.Transaction, lw.wallet.ChainParams())
 	if err != nil {
 		log.Error(err)
 		return "", err
 	}
 
-	var ssGenVersion uint32
-	var lastBlockValid bool
-	var votebits string
-	if stake.IsSSGen(&mtx) {
-		ssGenVersion = voteVersion(&mtx)
-		lastBlockValid = voteBits(&mtx)&uint16(BlockValid) != 0
-		votebits = fmt.Sprintf("%#04x", voteBits(&mtx))
-	}
-
-	var tx = DecodedTransaction{
-		Hash:           fmt.Sprintf("%02x", reverse(hash[:])),
-		Type:           transactionType(wallet.TxTransactionType(&mtx)),
-		Version:        int32(mtx.Version),
-		LockTime:       int32(mtx.LockTime),
-		Expiry:         int32(mtx.Expiry),
-		Inputs:         decodeTxInputs(&mtx),
-		Outputs:        decodeTxOutputs(&mtx, lw.wallet.ChainParams()),
-		VoteVersion:    int32(ssGenVersion),
-		LastBlockValid: lastBlockValid,
-		VoteBits:       votebits,
-	}
 	result, _ := json.Marshal(tx)
 	return string(result), nil
-}
-
-func decodeTxInputs(mtx *wire.MsgTx) []DecodedInput {
-	inputs := make([]DecodedInput, len(mtx.TxIn))
-	for i, txIn := range mtx.TxIn {
-		inputs[i] = DecodedInput{
-			PreviousTransactionHash:  fmt.Sprintf("%02x", reverse(txIn.PreviousOutPoint.Hash[:])),
-			PreviousTransactionIndex: int32(txIn.PreviousOutPoint.Index),
-			AmountIn:                 txIn.ValueIn,
-		}
-	}
-	return inputs
-}
-
-func decodeTxOutputs(mtx *wire.MsgTx, chainParams *chaincfg.Params) []DecodedOutput {
-	outputs := make([]DecodedOutput, len(mtx.TxOut))
-	txType := stake.DetermineTxType(mtx)
-	for i, v := range mtx.TxOut {
-
-		var addrs []dcrutil.Address
-		var encodedAddrs []string
-		var scriptClass txscript.ScriptClass
-		if (txType == stake.TxTypeSStx) && (stake.IsStakeSubmissionTxOut(i)) {
-			scriptClass = txscript.StakeSubmissionTy
-			addr, err := stake.AddrFromSStxPkScrCommitment(v.PkScript,
-				chainParams)
-			if err != nil {
-				encodedAddrs = []string{fmt.Sprintf(
-					"[error] failed to decode ticket "+
-						"commitment addr output for tx hash "+
-						"%v, output idx %v", mtx.TxHash(), i)}
-			} else {
-				encodedAddrs = []string{addr.EncodeAddress()}
-			}
-		} else {
-			// Ignore the error here since an error means the script
-			// couldn't parse and there is no additional information
-			// about it anyways.
-			scriptClass, addrs, _, _ = txscript.ExtractPkScriptAddrs(
-				v.Version, v.PkScript, chainParams)
-			encodedAddrs = make([]string, len(addrs))
-			for j, addr := range addrs {
-				encodedAddrs[j] = addr.EncodeAddress()
-			}
-		}
-
-		outputs[i] = DecodedOutput{
-			Index:      int32(i),
-			Value:      v.Value,
-			Version:    int32(v.Version),
-			Addresses:  encodedAddrs,
-			ScriptType: scriptClass.String(),
-		}
-	}
-
-	return outputs
-}
-
-func voteVersion(mtx *wire.MsgTx) uint32 {
-	if len(mtx.TxOut[1].PkScript) < 8 {
-		return 0 // Consensus version absent
-	}
-
-	return binary.LittleEndian.Uint32(mtx.TxOut[1].PkScript[4:8])
-}
-
-func voteBits(mtx *wire.MsgTx) uint16 {
-	return binary.LittleEndian.Uint16(mtx.TxOut[1].PkScript[2:4])
-}
-
-func reverse(hash []byte) []byte {
-	for i := 0; i < len(hash)/2; i++ {
-		j := len(hash) - i - 1
-		hash[i], hash[j] = hash[j], hash[i]
-	}
-	return hash
-}
-
-func transactionType(txType wallet.TransactionType) string {
-	switch txType {
-	case wallet.TransactionTypeCoinbase:
-		return "COINBASE"
-	case wallet.TransactionTypeTicketPurchase:
-		return "TICKET_PURCHASE"
-	case wallet.TransactionTypeVote:
-		return "VOTE"
-	case wallet.TransactionTypeRevocation:
-		return "REVOCATION"
-	default:
-		return "REGULAR"
-	}
 }
 
 func (lw *LibWallet) UnspentOutputs(account uint32, requiredConfirmations int32, targetAmount int64) ([]*UnspentOutput, error) {
